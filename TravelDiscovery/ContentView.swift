fab9d4c4dfd577c7cb9a7dcccf9adf7093352efd
Yasser Ahmed <iyasser007@gmail.com>
2023-04-25 12:39:33 +0300

    Initial design for discover categories.

--- TravelDiscovery/ContentView.swift
+++ TravelDiscovery/ContentView.swift
@@ -9,18 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView {
+                DiscoverCategoriesView()
+            }
+            .navigationTitle("Discovery")
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiscoverCategoriesView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 8) {
+                ForEach(0..<5, id: \.self) { _ in
+                    VStack(spacing: 4) {
+                        Spacer()
+                            .frame(width: 50, height: 50)
+                            .background(Color.gray)
+                            .cornerRadius(.infinity)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                        Text("Art")
+                            .font(.system(size: 12, weight: .semibold))
+                    }
+                }
+            }
+            .padding(.horizontal)
+        }
     }
 }
