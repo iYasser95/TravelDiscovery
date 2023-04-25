3c8354179f41b6aaf6579ad230be9bfcdd11a828
Yasser Ahmed <iyasser007@gmail.com>
2023-04-25 12:15:29 +0300

    Initial Commit

---
+++ TravelDiscovery/TravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryApp.swift
+//  TravelDiscovery
+//
+//  Created by Apple on 25/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
